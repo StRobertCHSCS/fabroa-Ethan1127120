c82ac9eb7343c11c016fd93a621ab3bfa4b0584e
Ethan1127120 <ethan.sam22@ycdsbk12.ca>
2019-10-01 09:46:18 -0400

    submit practice

---
+++ Working/LiveHacks/LiveHack1/practice1.py
@@ -0,0 +1,16 @@
+"""
+Name: practice1.py
+Purpose: TBD
+
+Author: Ethan Sam
+Date: 01/10/2019
+"""
+# Get Fahrenheit from user
+fahrenheit = float(input("Enter temperature in fahrenheit:  "))
+
+# Convert Fahrenheit to Celsius
+celcius = 5/9*(fahrenheit-32)
+
+# Output Celsius
+print("Inputted temperature (f):  " + str(fahrenheit))
+print("Temperature in celcius:  " + str(celcius))
