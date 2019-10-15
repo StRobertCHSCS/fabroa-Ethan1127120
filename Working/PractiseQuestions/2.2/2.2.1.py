8b4db331ff6c5f70f0dd3ada1254c9676f21fc84
Ethan1127120 <ethan.sam22@ycdsbk12.ca>
2019-10-15 09:19:52 -0400

    If Statement Practise

---
+++ Working/PractiseQuestions/2.2/2.2.1.py
@@ -0,0 +1,15 @@
+"""
+Name: 2.2.1.py
+Purpose: If Statements 1
+
+Author: Ethan Sam
+Date: 15/10/2019
+"""
+# Boolean Value
+can_Juggle = True
+
+# If statements with variable
+if can_Juggle:
+    print("I can juggle!")
+else:
+    print("I can't juggle.")
---
+++ Working/PractiseQuestions/2.2/2.2.2.py
@@ -0,0 +1,16 @@
+"""
+Name: 2.2.2.py
+Purpose: If statement practise 2
+
+Author: Ethan Sam
+Date: 15/10/2019
+"""
+
+# Boolean Statement
+is_Raining = True
+
+# Final Statement
+if is_Raining:
+    print("I'm going to eat lunch inside!")
+else:
+    print("I'm going to eat lunch outside!")
